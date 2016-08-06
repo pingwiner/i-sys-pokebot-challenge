d318b8d7cc5d1c53886984d522046060c7050873
Ilya Melnikov <i486dx@yandex.ru>
2016-08-07 00:26:24 +0400

    done

--- PokeBot.js
+++ PokeBot.js
@@ -98,4 +98,64 @@
     BasePokeBot.prototype.compare = function () {
         throw 'Must be implemented by child object';
     };
+
+    function PokeBot() {
+        BasePokeBot.call(this);        
+    };
+
+    PokeBot.prototype = {};
+    PokeBot.prototype.__proto__ = BasePokeBot.prototype;    
+    
+
+    PokeBot.prototype.find = function(list, conditions) {
+        let result = [];
+        for (let pokemon of list) {
+            if (PokeBot.check(pokemon, conditions)) {
+                result.push(pokemon);
+            }
+        }
+        return result;
+    };
+
+    PokeBot.check = function(pokemon, conditions) {
+        for (let condition of condition) {            
+            for (var key in condition) {
+                if (!pokemon.hasOwnProperty(key) || (pokemon[key] != condition[key]) {
+                    return false;
+                }                
+            }
+        }
+        return true;
+    };
+
+    PokeBot.prototype.evolve = function(pokemon) {
+        let currentKind = PokeBot.findByKind(pokemon.kind);        
+        let newKind = PokeBot.findByKind(currentKind.evolve);
+        if (newKind == null) return null;
+        let result = Object.assign({}, pokemon);
+        return Object.assign(result, newKind);        
+    };
+
+    PokeBot.findByKind = function(kind) {
+        for (let pokemon of BasePokeBot.prototype.pokedex) {
+            if (pokemon.kind === kind) return pokemon;
+        }
+        return null;
+    };
+
+    PokeBot.prototype.compare = function(...pokemons) {        
+        for (let item of pokemons) {
+            if (!PokeBot.compare2(this, item)) return false;
+        }
+        retunr true;
+    };
+
+    PokeBot.compare2 = function(pokemon1, pokemon2, step2 = false) {
+        for (var key in pokemon1) {
+            if (!pokemon2.hasOwnProperty(key) || (pokemon2[key] != pokemon1[key])) return false;
+        }
+        if (step2) return true;
+        return PokeBot.compare2(pokemon2, pokemon1, true);
+    };
+
 })();
